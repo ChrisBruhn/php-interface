fe1f65279916d59da95095b9cab97c73855d13ea
chbr-easv <chbr@easv.dk>
2017-04-13 10:21:09 +0200

    filename change

--- php-class.php
+++ php-class.php
@@ -6,17 +6,6 @@
  * Time: 12:06
  */
 
-// init the object of class with value 5
-$c = new class_name(5);
-// print to hole object to screen
-$c->printObject();
-// demonstration of set function
-$c->__set('test',100);
-// print to hole object to screen
-$c->printObject();
-
-// demostration of __get function
-echo "Return by __get function ".$c->__get('parameter')."<br>";
 
 // init the object of class
 $mybeer = new beer(50);
@@ -29,32 +18,6 @@ $mybeer->printObject();
 
 
 
-echo "<br>";
-
-
-class class_name
-{
-    public $parameter;
-
-    function __construct($parm){
-        $this->parameter = $parm;
-        echo "it has been set with the value $this->parameter ";
-
-    }
-
-    function __destruct(){}
-
-    function __get($parameter){
-        return $this->parameter;
-    }
-    function __set($parm, $value){
-        $this->parameter = $value;
-    }
-    function printObject (){
-        var_dump ($this);
-    }
-
-}
 
 class alcoholicdrinks{
     private $hangover;
